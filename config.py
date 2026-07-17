763e1f8622dcde75abe708ffc86b2af3a6d5110f
Дмитрий Крук <dimasik@MacBook-Air-Dmitrij.local>
2026-07-17 14:50:34 +0300

    Fix config.py for PostgreSQL

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 import os
-from pathlib import Path
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent
-DB_PATH = BASE_DIR / "warehouse.db"
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
+DB_URL = os.getenv("DATABASE_URL")  # Используем PostgreSQL
 
 def get_bot_token() -> str:
     if not BOT_TOKEN:
